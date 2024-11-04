c5e3d3d4825b3150b8dc1409d08dfd2432a277bb
WilliamChen114565006 <william.j.chen@stonybrook.edu>
2024-11-04 03:54:48 -0500

    Implemented mongodb database into Spring

--- src/main/java/com/spring/ServerController.java
+++ src/main/java/com/spring/ServerController.java
@@ -1,4 +1,5 @@
 package com.spring;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
@@ -6,13 +7,28 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.Repository.LARepo;
+import com.Model.Feature;
+
 import org.springframework.web.bind.annotation.PathVariable;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.util.List;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:3000")
 public class ServerController {
+
+    @Autowired
+    private LARepo laRepo;
+
+    // Get all Features
+    @GetMapping("/test")
+    public List<Feature> getAllFeatures() {
+        return laRepo.findAll();
+    }
+
 
     @GetMapping("/Data/{param1}/{param2}/{param3}")
     public ResponseEntity<String> getGeoJson(@PathVariable String param1, @PathVariable String param2, @PathVariable String param3) throws IOException {
